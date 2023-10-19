da376e180c8403103092549be59a0fbaacc4c351
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-10-19 02:30:47 -0700

    Updated the function that sorts an array of integers in ascending order using the Bubble sort algorithm.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,19 +9,24 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
+	int temp, swapped;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
+			swapped = 0;
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				swapped = 1;
 				print_array(array, size);
 			}
 		}
+
+		if (swapped == 0)
+			break;
 	}
 }
