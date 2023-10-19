f65e588e95133dfd5c245a281c74999c01ff2409
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-10-19 05:34:58 -0700

    Added a swap functiom

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,18 @@
 #include "sort.h"
 
 /**
+ * swap - Swaps the values of two integer pointers.
+ * @ap: Pointer to the first integer.
+ * @bp: Pointer to the second integer.
+ */
+void swap(int *ap, int *bp)
+{
+	int temp = *ap;
+	*ap = *bp;
+	*bp = temp;
+}
+
+/**
  * bubble_sort - Sorts an array of integers in ascending order using the
  * Bubble sort algorithm.
  * @array: The array to sort.
