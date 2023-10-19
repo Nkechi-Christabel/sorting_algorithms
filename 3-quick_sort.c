3035468368c42359df37b7a9193ea011a6c11c66
bammietop03 <bammietop@gmail.com>
2023-10-19 16:30:10 +0100

    sorting_algorithms

---
+++ 3-quick_sort.c
@@ -0,0 +1,69 @@
+#include "sort.h"
+
+/**
+ * lomuto_partition - Lomuto partition scheme
+ * @array: The array to be sorted
+ * @low: The starting index
+ * @high: The ending index
+ * @size: The size of the array
+ *
+ * Return: The index of the pivot element
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
+
+	if (i + 1 != high)
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
+
+	return (i + 1);
+}
+
+/**
+ * quicksort - Implementation of the Quick Sort algorithm
+ * @array: The array to be sorted
+ * @low: The starting index
+ * @high: The ending index
+ * @size: The size of the array
+ */
+void quicksort(int *array, int low, int high, size_t size)
+{
+	if (low < high)
+	{
+		int pivot = lomuto_partition(array, low, high, size);
+
+		quicksort(array, low, pivot - 1, size);
+		quicksort(array, pivot + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Quick Sort algorithm using Lomuto partition scheme
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quicksort(array, 0, size - 1, size);
+}
