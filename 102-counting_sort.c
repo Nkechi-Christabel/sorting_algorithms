7bc2265525f8d955feb84d00b383b3e7de01070d
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-10-20 03:56:41 -0700

    Updated conting sort function.

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,52 +1,49 @@
 #include "sort.h"
+
 /**
- * counting_sort -Sorts an arrayof integers
- * in ascending order using the
- * Counting sort algorithm
- * @array: array
- * @size: size
- * Return: no return
+ * counting_sort - Corts an array of integers in ascending order using the
+ * Counting sort algorithm.
+ * @array: The array of integers to be sorted.
+ * @size: The size or length of the array.
  */
 void counting_sort(int *array, size_t size)
 {
-	int n, i;
-	int *buff, *a;
+	int *count_arr, *sorted, i, max_num = 0;
 
-	if (size < 2)
+	if  (size < 2)
 		return;
 
-	for (n = i = 0; i < (int)size; i++)
-		if (array[i] > n)
-			n = array[i];
-
-	buff = malloc(sizeof(int) * (n + 1));
-	if (!buff)
-		return;
-
-	for (i = 0; i <= n; i++)
-		buff[i] = 0;
 	for (i = 0; i < (int)size; i++)
-		buff[array[i]] += 1;
-	for (i = 1; i <= n; i++)
-		buff[i] += buff[i - 1];
-
-	print_array(buff, (n + 1));
-	a = malloc(sizeof(int) * (size + 1));
-
-	if (!a)
 	{
-		free(buff);
+		if (array[i] > max_num)
+			max_num = array[i];
+	}
+	count_arr = malloc((max_num + 1) * sizeof(int));
+	if (count_arr == NULL)
+		return;
+
+	for (i = 0; i <= max_num; i++)
+		count_arr[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		count_arr[array[i]]++;
+	for (i = 0; i <= max_num; i++)
+		count_arr[i] += count_arr[i - 1];
+
+	print_array(count_arr, max_num + 1);
+	sorted = malloc(sizeof(int) * size);
+	if (sorted == NULL)
+	{
+		free(count_arr);
 		return;
 	}
 	for (i = 0; i < (int)size; i++)
 	{
-		a[buff[array[i]] - 1] = array[i];
-		buff[array[i]] -= 1;
+		sorted[count_arr[array[i] - 1]] = array[i];
+		count_arr[array[i]]--;
 	}
-
 	for (i = 0; i < (int)size; i++)
-		array[i] = a[i];
+		array[i] = sorted[i];
 
-	free(buff);
-	free(a);
+	free(sorted);
+	free(count_arr);
 }
