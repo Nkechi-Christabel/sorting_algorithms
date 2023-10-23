10b75cda572b68ea0999e97056ec0f98bb1a1cfb
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-10-23 16:36:05 -0700

    Wrote a function that sorts a deck of cards.

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,85 +1,122 @@
 #include "deck.h"
 
 /**
- * card_value - Get the numerical value of a card's string representation
- * @value: The string representation of the card's value
+ * sort_deck - Sorts a 52 card deck
+ * @deck: The deck to sort
  *
- * Return: The numerical value of the card
- */
-int card_value(const char *value)
-{
-	if (strcmp(value, "Ace") == 0)
-		return (1);
-
-	if (strcmp(value, "Jack") == 0)
-		return (11);
-
-	if (strcmp(value, "Queen") == 0)
-		return (12);
-
-	if (strcmp(value, "King") == 0)
-		return (13);
-
-	return (atoi(value));
-}
-
-/**
- * swap_nodes - Swap two adjacent nodes in the deck
- * @node1: The first node to swap
- * @node2: The second node to swap
- */
-void swap_nodes(deck_node_t *node1, deck_node_t *node2)
-{
-	if (node1 == NULL || node2 == NULL)
-		return;
-
-	if (node1->prev != NULL)
-		node1->prev->next = node2;
-
-	if (node2->next != NULL)
-		node2->next->prev = node1;
-
-	node1->next = node2->next;
-	node2->prev = node1->prev;
-
-	node1->prev = node2;
-	node2->next = node1;
-}
-
-/**
- * sort_deck - Sort the deck of cards by value and then by kind
- * @deck: A double pointer to the deck of cards
+ * Return: Always 0 (ok)
  */
 void sort_deck(deck_node_t **deck)
 {
-	int swapped;
-	deck_node_t *ptr1, *lptr = NULL;
-
-	if (deck == NULL || *deck == NULL || (*deck)->next == NULL)
+	if (deck == NULL || *deck == NULL)
 		return;
-
-	do {
-		swapped = 0;
-		ptr1 = *deck;
-
-		while (ptr1->next != lptr)
-		{
-			if (card_value(ptr1->card->value) > card_value(ptr1->
-				next->card->value) || (card_value(ptr1->card->value)
-					== card_value(ptr1->next->card->value) &&
-					ptr1->card->kind > ptr1->next->card->kind))
-			{
-				swap_nodes(ptr1, ptr1->next);
-
-				if (*deck == ptr1)
-					*deck = ptr1->prev;
-
-				swapped = 1;
-			}
-			else
-				ptr1 = ptr1->next;
-		}
-
-		lptr = ptr1;
-	} while (swapped);
+	sort_suit(deck);
+	sort_val(deck);
 }
+
+/**
+ * sort_suit - Sorts a 52 card deck by suit
+ * @deck: The deck to sort
+ */
+void sort_suit(deck_node_t **deck)
+{
+	deck_node_t *forward, *tmp;
+
+	for (forward = (*deck)->next; forward && forward->prev;
+			forward = forward->next)
+	{
+		for (; forward && forward->prev && forward->card->kind <
+				forward->prev->card->kind; forward =
+				forward->prev)
+		{
+			tmp = forward->prev;
+			swap(deck, tmp, forward);
+			forward = forward->next;
+		}
+	}
+}
+
+/**
+ * sort_val - Sorts a 52 card deck statically by value
+ * @deck: Deck to sort
+ */
+void sort_val(deck_node_t **deck)
+{
+	deck_node_t *f, *tmp;
+
+	for (f = (*deck)->next; f && f->prev; f = f->next)
+	{
+		for (; f && f->prev && vcmp(f->card, f->prev->card) &&
+				f->card->kind == f->prev->card->kind;
+				f = f->prev)
+		{
+			tmp = f->prev;
+			swap(deck, tmp, f);
+			f = f->next;
+		}
+	}
+}
+
+/**
+ * swap - Swaps 2 consecutive cards of a doubly linked list
+ * Used in the insertion algorithm
+ * @deck: Head node for the deck
+ * @card1: The first card to swap
+ * @card2: The second card to swap
+ */
+void swap(deck_node_t **deck, deck_node_t *card1, deck_node_t *card2)
+{
+	deck_node_t *prev, *next;
+
+	prev = card1->prev;
+	next = card2->next;
+
+	if (prev != NULL)
+		prev->next = card2;
+	else
+		*deck = card2;
+
+	card1->prev = card2;
+	card1->next = next;
+	card2->prev = prev;
+	card2->next = card1;
+
+	if (next)
+		next->prev = card1;
+}
+
+/**
+ * vcmp - Compares 2 card values
+ * @card1: The first card to compare
+ * @card2: The second card to compare
+ *
+ * Return: 1 if less than, 0 if greater or equal to
+ */
+int vcmp(const card_t *card1, const card_t *card2)
+{
+	char char1 = card1->value[0], char2 = card2->value[0];
+	const char *ord[14] = {"Ac", "1", "2", "3", "4", "5", "6",
+		"7", "8", "9", "10", "Ja", "Qu", "Ki"};
+	int idx1 = 0, idx2 = 0, i;
+
+	if ((char1 >= '0' && char1 <= '9') && (char2 >= '1' && char2 <= '9'))
+	{
+		if (card1->value[1] == '0')
+			char1 = '1' + 9;
+		if (card2->value[1] == '0')
+			char2 = '1' + 9;
+		return (char1 < char2);
+	}
+	else
+	{
+		for (i = 0; i < 14; i++)
+		{
+			if (card1->value[0] == ord[i][0] && card1->value[1] == ord[i][1])
+				idx1 = i;
+			if (card2->value[0] == ord[i][0] && card2->value[1] == ord[i][1])
+				idx2 = i;
+		}
+		return (idx1 < idx2);
+	}
+}
+
--- deck.h
+++ deck.h
@@ -1,16 +1,22 @@
 #ifndef DECK_H
 #define DECK_H
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * enum kind_e - Enumeration of card suits.
+ * @SPADE: Spades suit.
+ * @HEART: Hearts suit.
+ * @CLUB: Clubs suit.
+ * @DIAMOND: Diamonds suit.
+ */
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -22,8 +28,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -35,14 +41,15 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 
 void sort_deck(deck_node_t **deck);
-void sort_value(deck_node_t **deck);
-void swap(deck_node_t *temp, int i);
-int check_value(const char *string);
+void sort_suit(deck_node_t **deck);
+void sort_val(deck_node_t **deck);
+void swap(deck_node_t **deck, deck_node_t *card1, deck_node_t *card2);
+int vcmp(const card_t *card1, const card_t *card2);
 
 #endif /* DECK_H */
